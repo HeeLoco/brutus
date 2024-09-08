baf30621a2f4a9f1e97d4e3af480cdfd209d997f
HeeLoco <HeeLoco>
2024-09-08 11:05:29 +0000

    order imports, define test enpoints, webservice start to end

--- src/server/az/main.py
+++ src/server/az/main.py
@@ -1,49 +1,66 @@
-import uvicorn
-from fastapi import FastAPI
-
 import os
+import uvicorn
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
+from enum import Enum
+from fastapi import FastAPI
 
+#local
 from library.resource_groups import resource_group_create_or_update, resource_group_show
+
+class testEndpoints(str, Enum):
+    me         = "me"
+    connection = "connection"
+    # all        = "all"
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000, host="0.0.0.0")
-
 # Acquire a credential object using DevaultAzureCredential.
-credential = DefaultAzureCredential()
+# credential = DefaultAzureCredential()
 
 # Retrieve subscription ID from environment variable.
-subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+# subscription_id = "xxx" #os.environ["AZURE_SUBSCRIPTION_ID"]
 
 # Obtain the management object for resources.
-resource_client = ResourceManagementClient(credential, subscription_id)
+# resource_client = ResourceManagementClient(credential, subscription_id)
 
-@app.get("/testme")
-def testme_function():
-    return {"result":"nice"}
+@app.get("/test/{test_endpoint}")
+def tests(test_endpoint: testEndpoints):
+    match test_endpoint:
+        case testEndpoints.me:
+            return {"result":"nice"}
+        
+        case testEndpoints.connection:
+            return {"connected":True}
+    # if test_endpoint is testEndpoints.me:
+    #     return {"result":"nice"}
+    
+    # if test_endpoint is testEndpoints.connection:
+    #     return {"connected":True}
 
 @app.post("/resource_group/create")
-def resource_group_create(name, location, tags={}):
+def create_resource_group(name, location, tags={}):
     return resource_group_create_or_update(name, location, resource_client, tags)
 
 @app.post("/resource_group/update")
-def resource_group_create(name, location, tags={}):
+def update_resource_group(name, location, tags={}):
     #additional checks before updating?
     return resource_group_create_or_update(name, location, resource_client, tags)
 
 @app.get("/resource_group/list")
-def resource_group_list(resource_client):
+def list_resource_groups(resource_client):
     return resource_client.resource_groups.list()
 
-@app.get("/resource_group/get_resources")
-def resource_group_show(name, resource_client):
+@app.get("/resource_group/show/{name}")
+def resource_group_show(name: str, resource_client):
     return resource_group_show(name, resource_client)
 
 @app.get("/resource_group/get_resources_expandBy")
 def resource_group_show(name, resource_client, expandBy):
     #check given expandBy argument
     return resource_group_show(name, resource_client, expandBy)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
