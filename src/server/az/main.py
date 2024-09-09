700104bbf6c809242c8a1e856e1dd5884e6a02a6
HeeLoco <HeeLoco>
2024-09-09 11:44:11 +0000

    add http exception and raise, add tags, add descriptions

--- src/server/az/main.py
+++ src/server/az/main.py
@@ -4,7 +4,7 @@ import uvicorn
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 #local
@@ -37,8 +37,15 @@ def gen_resource_client(resource_client_info: resourceClientInfo):
     #custom validation(s)
     return ResourceManagementClient(resource_client_info.credential, resource_client_info.subscription_id, resource_client_info.api_version, resource_client_info.base_url)
 
-@app.post("/test/{test_endpoint}")
+@app.post("/test/{test_endpoint}", tags=["test"])
 def tests(test_endpoint: testEndpoints, resource_client_info: resourceClientInfo | None = None):
+    """
+    Endpoints for testing purposes are:
+
+    - **me**: own sample response
+    - **connection**: connection / reachablility test
+    - **clientinfo**: needed client information to get access to desired connection
+    """
     match test_endpoint:
         case testEndpoints.me:
             return {"result":"nice"}
@@ -49,7 +56,8 @@ def tests(test_endpoint: testEndpoints, resource_client_info: resourceClientInfo
         
         case testEndpoints.clientinfo:
             if not resource_client_info:
-                return {"result":"no body"}
+                # return {"result":"no body"}
+                raise HTTPException(status_code=420, detail="body is missing")
             #send back plain 
             return resource_client_info.model_dump()
         
@@ -63,24 +71,26 @@ def tests(test_endpoint: testEndpoints, resource_client_info: resourceClientInfo
 
 
 
-@app.post("/resource_group/create")
+@app.post("/resource_group/create", 
+          tags=["resource group"],
+          response_description="The created group",)
 def create_resource_group(name, location, tags={}):
     return resource_group_create_or_update(name, location, resource_client, tags)
 
-@app.post("/resource_group/update")
+@app.post("/resource_group/update", tags=["resource group"])
 def update_resource_group(name, location, tags={}):
     #additional checks before updating?
     return resource_group_create_or_update(name, location, resource_client, tags)
 
-@app.get("/resource_group/list")
+@app.get("/resource_group/list", tags=["resource group"])
 def list_resource_groups(resource_client):
     return resource_client.resource_groups.list()
 
-@app.get("/resource_group/show/{name}")
+@app.get("/resource_group/show/{name}", tags=["resource group"])
 def resource_group_show(name: str, resource_client):
     return resource_group_show(name, resource_client)
 
-@app.get("/resource_group/get_resources_expandBy")
+@app.get("/resource_group/get_resources_expandBy", tags=["resource group"])
 def resource_group_show(name, resource_client, expandBy):
     #check given expandBy argument
     return resource_group_show(name, resource_client, expandBy)
