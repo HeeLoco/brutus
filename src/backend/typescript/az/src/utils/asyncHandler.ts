3e09a518786124e1e0a7b06181714f130dfa4744
HeeLoco <HeeLoco>
2025-08-01 10:06:36 +0200

    stoped in dead end and need a fresh restart

--- src/backend/typescript/az/src/utils/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<void>;
-
-export function asyncHandler(fn: AsyncFunction) {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
